67b7be54d14210f6e20fe93e1714ac9fb74952ba
Victor Rios Saucedo <36146644+Rotooo@users.noreply.github.com>
2024-07-26 21:14:27 -0600

    update 26-07-2024
    changs

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../assets/styles/styles.css';
 
 export default function Calculator() {
-  const [display, setDisplay] = useState('');
+  const [display, setDisplay] = useState('0');
 
   const handleClick = (value) => {
     if (display === '0') {
@@ -28,23 +28,39 @@ export default function Calculator() {
     <div className="calculator">
       <input type="text" value={display} readOnly className='opText' />
       <div className="buttons">
-        <button className='btnNum' onClick={() => handleClick('7')}>7</button>
-        <button className='btnNum' onClick={() => handleClick('8')}>8</button>
-        <button className='btnNum' onClick={() => handleClick('9')}>9</button>
-        <button className='btnOp' onClick={() => handleClick('+')}>+</button>
-        <button className='btnNum' onClick={() => handleClick('4')}>4</button>
-        <button className='btnNum' onClick={() => handleClick('5')}>5</button>
-        <button className='btnNum' onClick={() => handleClick('6')}>6</button>
-        <button className='btnOp' onClick={() => handleClick('-')}>-</button>
-        <button className='btnNum' onClick={() => handleClick('1')}>1</button>
-        <button className='btnNum' onClick={() => handleClick('2')}>2</button>
-        <button className='btnNum' onClick={() => handleClick('3')}>3</button>
-        <button className='btnOp' onClick={() => handleClick('*')}>*</button>
-        <button className='btnNum' onClick={() => handleClick('0')}>0</button>
-        <button className='btnOp' onClick={() => handleClick('.')}>.</button>
-        <button className='btnCalc' onClick={calculate}>=</button>
+      <div className='numDiv'>
         <button className='btnClear' onClick={clear}>C</button>
-        <button className='btnOp' onClick={() => handleClick('/')}>/</button>
+        <button className='btnNum' onClick={() => handleClick('3.1416')}>π</button>
+          <button 
+            className='btnNum' 
+            onClick={() => 
+              handleClick('2.7182818284590452353602874713527')}>e</button>
+        <button className='btnOp' onClick={() => handleClick('/')}>÷</button>
+      </div>
+        <div className='numDiv'>
+          <button className='btnNum' onClick={() => handleClick('7')}>7</button>
+          <button className='btnNum' onClick={() => handleClick('8')}>8</button>
+          <button className='btnNum' onClick={() => handleClick('9')}>9</button>
+          <button className='btnOp' onClick={() => handleClick('*')}>x</button>
+        </div>
+        <div className='numDiv'>
+          <button className='btnNum' onClick={() => handleClick('4')}>4</button>
+          <button className='btnNum' onClick={() => handleClick('5')}>5</button>
+          <button className='btnNum' onClick={() => handleClick('6')}>6</button>
+          <button className='btnOp' onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div className='numDiv'>
+          <button className='btnNum' onClick={() => handleClick('1')}>1</button>
+          <button className='btnNum' onClick={() => handleClick('2')}>2</button>
+          <button className='btnNum' onClick={() => handleClick('3')}>3</button>
+          <button className='btnOp' onClick={() => handleClick('+')}>+</button>
+        </div>
+        <div className='numDiv'>
+          <button className='btnNum' onClick={() => handleClick('0')}>0</button>
+          <button className='btnOp' onClick={() => handleClick('.')}>.</button>
+          <button className='btnOp' onClick={() => handleClick('**')}>x˟</button>
+          <button className='btnCalc' onClick={calculate}>=</button>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from 'react-dom/client';
 import HolaMundo from "./components/HolaMundo";
 
-ReactDOM.render(<HolaMundo />, document.getElementById("app"));
+const root = ReactDOMClient.createRoot(document.getElementById("app"));
+root.render(
+  <React.StrictMode>
+    <HolaMundo />
+  </React.StrictMode>
+);
